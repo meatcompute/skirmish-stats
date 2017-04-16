59011570be7ac7836c5bda7b040541b7dcaf69d4
Mikaela Patella <m.c.patella@gmail.com>
2017-04-16 13:01:14 -0700

    remove url from project description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject skirmish-stats "0.1.0"
   :description "Analytics tools for competitive EVE Online players."
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
