8443cf4d886347f4067fe8adb0bb0afeb9cc0f61
Mikaela Patella <m.c.patella@gmail.com>
2017-04-21 13:05:03 -0700

    add datomic client

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.datomic/clj-client "0.8.606"]
                  [hiccup "1.0.5"]
                  [yesql "0.5.3"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
