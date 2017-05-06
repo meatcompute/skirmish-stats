d7aaee32490c3d797468ccbc263f0f37bbd58b9d
Mikaela Patella <m.c.patella@gmail.com>
2017-05-05 20:46:31 -0700

    add core.async to project

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]
                  [com.datomic/clj-client "0.8.606"]
                  [hiccup "1.0.5"]
                  [yesql "0.5.3"]
