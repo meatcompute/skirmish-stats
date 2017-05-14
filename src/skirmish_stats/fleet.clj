618498e00ebbd905bf536149f725fba41d79c55d
Mikaela Patella <m.c.patella@gmail.com>
2017-05-14 01:24:04 -0700

    adjust spec on fleet

--- src/skirmish_stats/fleet.clj
+++ src/skirmish_stats/fleet.clj
@@ -1,18 +1,15 @@
 (ns skirmish-stats.fleet)
 
-(defn composition-spec []
-  {:composition/pilot java.lang.String
-   :composition/ship java.lang.String
-   :composition/points java.lang.Long
-   :composition/dps java.lang.Double})
+(defn slot-spec []
+  {:slot/pilot java.lang.String
+   :slot/ship java.lang.String
+   :slot/points java.lang.Long
+   :slot/dps java.lang.Double})
 
 (defn spec []
   {::id java.lang.Long
    ::name java.lang.String
    ::points java.lang.Long
    ::point-limit java.lang.Long
-   ::slot [composition]})
-
-(defn team []
-  ::id java.lang.Long
-  )
+   ::slot-limit java.lang.Long
+   ::slot [slot-spec]})
